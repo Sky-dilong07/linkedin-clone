e6b197f36466be60b4d0ea890ad28401dbc9b74d
Saurabh Mishra <saurabhmishra12mgs@gmail.com>
2025-11-08 15:18:48 +0530

    Update test_db.php

--- test_db.php
+++ test_db.php
@@ -1,15 +1,4 @@
 <?php
-$host = "127.0.0.1";
-$user = "root";
-$pass = "";  // no password (phpMyAdmin opens without)
-$db   = "linkedin_clone";
-$port = 3307;
-
-$conn = new mysqli($host, $user, $pass, $db, $port);
-
-if ($conn->connect_error) {
-    die("❌ Connection failed: " . $conn->connect_error);
-}
-
-echo "✅ Connected successfully to MySQL!";
+include("db.php");
+echo "✅ Database connected successfully!";
 ?>
